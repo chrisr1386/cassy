7353d4b22a31233bbd2c7b4f5ee1d54598a9e756
Chris Root <chrisr1386@gmail.com>
2011-12-07 15:26:01 -0500

    send extra attributes to consumer

--- lib/cassy/authenticators/devise.rb
+++ lib/cassy/authenticators/devise.rb
@@ -5,7 +5,7 @@ module Cassy
       def self.find_user(credentials)
         # Find the user with the given email
         method = "find_by_#{Cassy.config[:username_field] || 'email'}"
-        User.send(method, credentials[:username])
+        User.send(method, credentials[:username], credentials[:email])
       end
       
       def self.find_user_from_ticket(ticket)
